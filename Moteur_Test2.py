206b065f8d4dcca9b7520c6e317fc0084457bc2b
theocrays1-opps <theocrays82@gmail.com>
2026-02-02 17:21:25 +0100

    Update Moteur_Test2.py

--- Moteur_Test2.py
+++ Moteur_Test2.py
@@ -8,8 +8,8 @@ def pwm_out(t):
     global n1
     global n2
 
-    out1.value(n1)
-    out2.value(n2)
+    out1.value(n2)
+    out2.value(n1)
     if n1==0 and n2==0:
         n1=0
         n2=1
@@ -27,7 +27,7 @@ n1=0
 n2=0 
  
 tim1 = Timer(1)
-tim1.init(period=500, mode=Timer.PERIODIC, callback=pwm_out)
+tim1.init(period=20, mode=Timer.PERIODIC, callback=pwm_out)
 
 # Configuration des broches
 dir1 = Pin(16, Pin.OUT)   # Direction
@@ -37,10 +37,10 @@ out2 = Pin(19, Pin.OUT)
 #pwm1 = PWM(Pin(17), freq=10000, duty=512) # create PWM object from a pin
 #pwm2 = PWM(Pin(19), freq=10000, duty=512) # create PWM object from a pin
 
-dir1.value(0)
-dir2.value(0) 
+dir1.value(1)
+dir2.value(1) 
 time.sleep(1)
 
 while True:
     pass
-    
+
