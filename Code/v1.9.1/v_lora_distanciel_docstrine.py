c888e4e6e0144c80a196e88a10bca942629bdc0d
matheis michalak <m.matheis2005@gmail.com>
2026-03-27 22:47:26 +0100

    Enhance documentation with detailed docstrings
    Added detailed docstrings for class methods to improve documentation and understanding of the code.

--- Code/v1.9.1/v_lora_distanciel_docstrine.py
+++ Code/v1.9.1/v_lora_distanciel_docstrine.py
@@ -3,7 +3,20 @@ import time, random
 
 class LORAE32:
     def __init__(self, uart_id, tx_pin, rx_pin, m0_pin, m1_pin, aux_pin):
-        # Configuration matérielle
+        """
+        initialisation materielle :
+
+        variable entrée :
+        uart-id : numéro de l'uart utilisé [1;2]
+        tx_pin : numéro du tx de l'esp32
+        rx_pin : numéro du rx de l'esp32
+        m0_pin : numéro du m0 de l'esp32
+        m1_pin : numéro du m1 de l'esp32
+        aux_pin : numéro de l'AUX de l'esp32
+
+        self.set_mode(0) : mode initialisé en mode normal
+
+        """
         self.uart = UART(uart_id, baudrate=9600, tx=tx_pin, rx=rx_pin, timeout=200)
         self.m0 = Pin(m0_pin, Pin.OUT)
         self.m1 = Pin(m1_pin, Pin.OUT)
@@ -11,11 +24,32 @@ class LORAE32:
         self.set_mode(0) # Mode normal par défaut
 
     def wait_aux(self):
+        """
+        controle du signal AUX :
+
+        used method :
+        self.aux.value() : controle de la mise a 1 de l'AUX pour etre sur de la liberté d'envoi
+        time.sleep_ms() : timing de sécurité
+
+        """
         while self.aux.value() == 0:
             time.sleep_ms(1)
         time.sleep_ms(2)
 
     def set_mode(self, mode):
+        """
+        configuration des modes :
+
+        used method :
+        self.wait.aux() : vérification de la liberté du module 
+        self.m0.value() : mise a 0 ou 1 de m0 selon le mode souhaité
+        self.m1.value() : mise a 0 ou 1 de m0 selon le mode souhaité
+        [ m0=0 ; m1=0 ] = mode 0 ; mode normal
+        [ m0=0 ; m1=1 ] = mode 1 ; mode wake-up
+        [ m0=1 ; m1=0 ] = mode 2 ; mode power-saving
+        [ m0=1 ; m1=1 ] = mode 3 ; mode sleep/config
+
+        """
         self.wait_aux()
         if mode == 0:   # NORMAL
             self.m1.value(0); self.m0.value(0)
@@ -25,10 +59,23 @@ class LORAE32:
             self.m1.value(1); self.m0.value(0)
         elif mode == 3: # SLEEP / CONFIG
             self.m1.value(1); self.m0.value(1)
-        time.sleep_ms(20)
         self.wait_aux()
 
     def setup_config(self, addr_h, addr_l, channel):
+        """
+        variable entrée :
+        addr_h : octet adresse haute du module emetteur
+        addr_l : octet adresse basse du module emetteur
+        channel : entier du canal du module emetteur
+
+        used method :
+        self.set_mode(3) : mise en mode config 
+        bytes([0xC0, addr_h, addr_l, 0x1A, channel, 0xC4]) : trame de config du module emetteur
+        self.uart.write(config_cmd) : envoyer la trame au module pour configuration
+        self.uart.any() - self.uart.read() : lecture de la reponse du module 
+        self.set_mode(0) : remise en mode normal pour la suite
+
+        """
         self.set_mode(3)
         config_cmd = bytes([0xC0, addr_h, addr_l, 0x1A, channel, 0xC4])
         self.uart.write(config_cmd)
@@ -40,6 +87,17 @@ class LORAE32:
         print(f"Configuration fixe appliquée sur canal {channel}")
         
     def get_trame(self):   # Création d'une trame sous la forme: $ val1 # val2 # texte #  
+        """
+        creation de trame aléatoire pour test : 
+
+        used method :
+        random.randint(100, 1000) : int aléatoire entre 100 et 1000 en val 1 et 2
+        ''.join(random.choice(alphabet) for _ in range(4)) : 4 char aléatoire dans le str alphabet
+
+        variable sortie :
+        return "$"+str(val1) + "#" + str(val2) + "#" + texte + "#" : concaténation de tout les éléments pour envoi
+
+        """
         alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         val1 = random.randint(100, 1000)
         val2 = random.randint(100, 1000)
@@ -50,6 +108,14 @@ class LORAE32:
         return "$"+str(val1) + "#" + str(val2) + "#" + texte + "#"
 
     def crc16(self, texte):
+        """
+        variable entrée :
+        texte : trame en sortie de get_trame
+
+        variable sortie :
+        crc16 : 2 octets pour controle
+        
+        """
         crc = 0xFFFF
         for char in texte:
             crc ^= ord(char)
@@ -61,7 +127,16 @@ class LORAE32:
         return f"{crc:04X}"
 
     def crc_valide(self, texte):
-        """ Calcule le CRC16 de la trame reçue et le compare à celui transmis """
+        """ 
+        Calcule le CRC16 de la trame reçue et le compare à celui transmis :
+
+        variable entrée :
+        texte : trame recu moins les 2 octets de crc16
+
+        variable sortie : 
+        return True or False selon la validité
+        
+        """
         # On extrait les 4 derniers caractères (le CRC envoyé)
         crc_recu = texte[-4:]
         # On calcule le CRC sur tout le début de la trame (tout sauf les 4 derniers car c le crc)
@@ -76,21 +151,41 @@ class LORAE32:
             return False
         
     def send_point_to_point(self, target_h, target_l, target_chan, message):
-        """ MODE 1 : Envoyer à un récepteur précis """
+        """ 
+        MODE 1 : Envoyer à un récepteur précis :
+        
+        variable entrée :
+        target_h : octet adresse haute du module recepteur
+        target_l : octet adresse basse du module recepteur
+        target_chan : entier du canal du module recepteur
+        message :  trame a envoyer 
+
+        used method : 
+        self.wait_aux : vérification de la liberté du module
+        bytes([target_h, target_l, target_chan]) : header de la trame a envoyer
+        self.uart.write(header + message.encode('utf-8')) : concaténation + envoie sur uart
+
+        """
         self.wait_aux()
         header = bytes([target_h, target_l, target_chan])
         self.uart.write(header + message.encode('utf-8'))
         print(f"Envoyé à {target_h:02x}{target_l:02x} sur canal {target_chan}")
 
     def send_broadcast(self, target_chan, message):
-        """ MODE 2 : Envoyer à TOUS les modules sur un canal précis (FFFF) """
+        """ 
+        MODE 2 : Envoyer à TOUS les modules sur un canal précis (FFFF) :
+        
+        """
         self.wait_aux()
         header = bytes([0xFF, 0xFF, target_chan])
         self.uart.write(header + message.encode('utf-8'))
         print(f"Broadcast envoyé sur canal {target_chan}")
 
     def decoupage(self, texte):
-        """ Analyse la trame reçue ($val1#val2#texte1#crc) et retourne un dictionnaire """
+        """ 
+        Analyse la trame reçue ($val1#val2#texte1#crc) et retourne un dictionnaire :
+        
+        """
         try:
             parties = texte[1:].split('#')# On enlève le '$' du début et on découpe par les '#'
                                                                 # texte[1:] permet de commencer après le premier caractère
@@ -107,6 +202,14 @@ class LORAE32:
         return None
 
     def receive(self):
+        """
+        used method : 
+        self.uart.any() - self.uart.read() : lecture de la reception du module
+
+        variable sortie :
+        return data : octet de reception
+        
+        """
         if self.uart.any:
             data =  self.uart.read()
             return data
@@ -115,6 +218,11 @@ class LORAE32:
     def send_trame(self, ad_mo_h, ad_mo_l, ad_mo_chanel):
         """
         envoie de la trame: 
+
+        variable entrée :
+        ad_mo_h : adresse haute du recepteur
+        ad_mo_l : adresse basse du recepteur
+        ad_mo_chanel : canal du recepteur
 
         used method:
         self.get_trame() : trame random for test
