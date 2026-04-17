18d489dac25c22174e930fdc176e05b79fba8fc2
matheis michalak <m.matheis2005@gmail.com>
2026-04-17 11:56:20 +0200

    Update tramage_officiel.py

--- Code/tramage_officiel.py
+++ Code/tramage_officiel.py
@@ -1,8 +1,10 @@
-def checkup_syst(nv_eau,nv_vanne,syst,meteo): 
-    #il faut regarder si tout fonctionne bien
-    # 0x0000:RAS / 0x0001:i2c / 0x0002:moteur / 0x0004:capteur 1 / 0x0008:capteur 2 / 0x0010:...
-    # 0%0000 -> 1er bit pour i2c , 2eme pour moteur , ...
-    return checkup #du type 0x0006 donc 0%0110 donc pb moteur et capteur 1
+def checkup_syst(flag_cpt1, flag_cpt2, flag_moteur):
+    #0x00 -> vert -> système parfait
+    #0x0[1-F] -> orange -> sytème endommager a verifier mais en état de fonctionnement
+    #0x1[1-F] -> rouge -> système endommager a réparer et non fonctionnel
+    """faire le dico du systeme avec key (id) 
+
+    return code_erreur
 
 def checkup_meteo(var):
     meteo = 0x00
@@ -12,7 +14,7 @@ def checkup_meteo(var):
     return meteo #du type 0xFF ou 0x00
 
 def get_trame(nv_eau, nv_vanne, syst, meteo, crc16 ):
-    """ $00#00#00#0#crc16"""
+    """ $00#00#00#00#crc16"""
     
     return "$"+ nv_eau + "#" + nv_vanne + "#" + syst + "#" + meteo + "#" + crc16
     
