e21574995b61b9d393451719f313b54c0f2ac9ad
matheis michalak <m.matheis2005@gmail.com>
2026-06-12 11:14:33 +0200

    Add files via upload

---
+++ Code/Slave_barrage_code+cpt_nonop.py
@@ -0,0 +1,312 @@
+from machine import UART, Pin, Timer, I2C
+from Slave_barrage_lib import *   # Fichier à télécharger dans l’ESP32 :  class LoRaE32 
+import time, random
+
+class Color:
+    R = '\033[31m'  # Rouge
+    G = '\033[32m'  # Vert
+    B = '\033[34m'  # Bleu
+    Y = '\033[33m'  # Jaune
+    END = '\033[0m' # Reset
+
+#********** init lora *********
+# Cablage de l'ESP32 sur le module Lora
+UART2 = 2  # deuxième port UART de l'ESP32
+RX    = 16
+TX    = 17
+AUX   = 22
+M0    = 19
+M1    = 18
+
+# Configuration des modules LoRa - Envoi simple de données en point à point
+# Adresse du module Lora sur le barrage 
+addr_high_barrage = 0x00
+addr_low_barrage  = 0x02
+# Adresse du module Lora sur le barrage de commande 
+addr_high_pupitre = 0x00
+addr_low_pupitre  = 0x03
+# Canal de communication
+CANAL = 0x06               # fréquence 868 MHz (canal 6)
+DEBIT = 9600               # Débit communication série
+CONF  = 0xC0               # mode fixe à 30 dBm 
+
+reception_flag = False     # interruption RX valide si données séries reçues
+emission_flag = False
+
+#Instanciation de la liaison serie LORA-ESP32 avec interruption
+uart = UART(UART2, baudrate=DEBIT, tx=TX, rx=RX)
+#Instanciation du module Lora
+lora_barrage = LoRaE32('900T30D', uart, AUX, M0, M1)
+lora_barrage.begin()
+
+# Configuration du module Lora
+lora_barrage.config['CHANNEL'] = CANAL              # fréquence 868 MHz (canal 6)
+lora_barrage.config['ADDR_H']  = addr_high_barrage  # adresse barrage MSB
+lora_barrage.config['ADDR_L']  = addr_low_barrage   # adresse barrage LSB
+lora_barrage.config['OPTION']  = CONF               # mode fixe à 30 dBm
+lora_barrage.set_config(save=True)
+
+#***************init capteur i2c ************
+i2c = I2C(0, sda=Pin(21), scl=Pin(23), freq=400000)
+
+dist1, dist2, dist3, hauteur = 0, 0, 0, 0 		# Stockage des 3 dernières valeurs et de la moyenne
+error, count = 0, 0 							# Valeurs des erreurs et du compteur
+max_error, max_marge = 2, 10   					# Nombre d'erreurs tolérées et variation maximale autorisée (en cm) entre deux mesures
+timer_period_ms, error_period_ms = 5000, 2000	# Fréquence de mesure normale / erreur détectée
+att_moy = 100									# Attente en ms entre chaque distance mesurée
+mesure_hauteur = 0
+prep = 2
+
+def get_firmware_version(addr):
+    """
+        Cette fonction permet de récupérer la version du capteur détecté.
+        On envoie d'abord la commande à l'adresse du capteur pour récupérer sa version puis on lit sa réponse.
+        On formate ensuite les octets qui nous intéresse dans une variable.
+        
+        Args:
+            addr (string): Adresse du capteur
+            
+        Returns:
+            string: Version du capteur trouvée
+    """
+    try:
+        # Commande version du firmware
+        i2c.writeto(addr, b'\x5a\x04\x01\x5f')
+        time.sleep_ms(100)
+        
+        # Lecture 7 octets de réponse
+        res = i2c.readfrom(addr, 7)
+        
+        # Vérification trame reçue (7 octets, octet 0 entête 0x5A, octet 2 confirmation ID commande 0x01)
+        if len(res) >= 7 and res[0] == 0x5a and res[2] == 0x01:
+            # Formatage version
+            return "V{}.{}.{}".format(res[3], res[4], res[5])
+    except:
+        pass
+    
+    return "Inconnue"
+
+
+print("Recherche de capteurs I2C...")
+# Scan bus I2C adresses matérielles connectées
+devices = i2c.scan()
+sensor_info = {} 								# Dictionnaire stock adresse et version capteur(s)
+
+if devices:
+    for d in devices:
+        version = get_firmware_version(d)
+        sensor_info[d] = version
+        print("Trouvé : Adresse {} | Version : {}".format(hex(d), version))
+    
+    # Première adresse trouvée = capteur principal
+    TFMINI_ADDR = devices[0]
+else:
+    # Echec scan = adresse par défaut (0x10 pour le TFMini Plus en mode I2C)
+    print("\nAucun capteur détecté. Utilisation adresse par défaut 0x10")
+    TFMINI_ADDR = 0x10
+
+def get_distance():
+    """
+        Cette fonction permet de récupérer la distance mesurée par un capteur.
+        La commande de lecture de distance est envoyée au capteur et sa réponse est stocker dans la variable data.
+        On vérifie qu'elle soit au bon format puis on récupère les octets indiquant la distance.
+        Un correctif est appliqué sur la distance mais n'est pas obligatoire sur tous les capteurs.
+        
+        Args:
+            ...
+            
+        Returns:
+            int: Distance mesurée en cm
+    """
+    try:
+        # Commande demande de mesure de distance
+        i2c.writeto(TFMINI_ADDR, b'\x5a\x05\x00\x01\x60')
+        
+        # Lecture 9 octets de données de mesure
+        data = i2c.readfrom(TFMINI_ADDR, 9)
+        
+        # Vérification protocole standard TFMini
+        if len(data) >= 9 and data[0] == 0x59 and data[1] == 0x59:
+            # Distance codée sur 2 octets
+            # Octet 2 (poids faible) + octet 3 (poids fort) décalé de 8 bits vers la gauche
+            distance = data[2] + (data[3] << 8)
+            distance = round(distance * 1.05)
+            return distance
+    except:
+        print("\nErreur dans la récupération de la mesure.")
+        return None
+    
+    return None
+
+
+def use_data():
+    """
+        Cette fonction affiche et gère les mesures.
+        On initialise toutes nos variables à la première mesure pour éviter une erreur en début de boucle.
+        On vérifie si la distance est prise en compte par le capteur et on compare la moyenne avec la précédente.
+        Une fois acceptée, on peut calculer une moyenne à partir de trois valeurs successives puis on affiche les informations.
+        Si la différence entre les deux dernières moyennes est plus élevée que la marge configurée, une erreur est comptée.
+        Trop d'erreur de suite indique qu'il s'agit d'une valeur correcte.
+        
+        Args:
+            timer (timer): Timer par interruption
+            
+        Returns:
+            ...
+    """
+    global dist1, dist2, dist3, hauteur, error, count, att_moy, prep
+    mesure = get_distance()
+        
+    if mesure is not None:
+        if dist1 == 0:
+            hauteur = mesure
+            dist1 = mesure
+            dist2 = mesure
+            dist3 = mesure
+        # Vérification mesure physiquement possible
+        if 10 < mesure < 1200:
+            
+            # Accepte la valeur si écart avec mesure précédente correct et erreurs maximales non atteintes
+            if abs(mesure - hauteur) <= max_marge:
+                
+                # Prise de mesure décalée pour calculer une moyenne
+                dist3 = mesure
+                
+                time.sleep_ms(att_moy)
+                mesure = get_distance()
+                # Sécurité au cas où une mesure intermédiaire renverrait None
+                if mesure is None: mesure = dist3
+                dist2 = mesure
+                
+                time.sleep_ms(att_moy)
+                mesure = get_distance()
+                if mesure is None: mesure = dist2
+                dist1 = mesure
+                
+                hauteur = round((dist1 + dist2 + dist3)/3)
+                
+                # Conversion en 2 octets
+                lsb = hauteur & 0xFF
+                msb = (hauteur >> 8) & 0xFF
+                octets_distance = bytes([lsb, msb])
+                
+                # Réinitialise le compteur d'erreurs car valeur valide
+                error = 0
+                
+                # Incrémente le compteur global
+                count += 1
+                
+                # Formatage pour l'affichage
+                addr_h = hex(TFMINI_ADDR)
+                ver = sensor_info.get(TFMINI_ADDR, "N/A")
+                
+                # Affiche les résultats
+                if count > prep:
+                    print("[Adresse: {} | Version: {}]\nDistance n°{}: {} cm\nFormat bytes: {}\n\n".format(addr_h, ver, count, hauteur, octets_distance))
+                
+                # Après réinitialisation erreurs: retour au cycle normal
+                if error == 0:
+                    timer_sensor.init(mode=Timer.PERIODIC, period=timer_period_ms, callback=interrupt_hauteur)
+                        
+                return hauteur
+            
+            else:
+                # Variance trop élevée = ignorée + erreur + temps de mesure raccourci
+                timer_sensor.init(mode=Timer.PERIODIC, period=error_period_ms, callback=interrupt_hauteur)
+                error += 1
+                if count > prep:
+                    print(f"{error} erreur(s)\n")
+            
+                if error > max_error:
+                    # Erreurs maximales atteintes = réinitialisation des variables + reprise du cycle normal
+                    if count > prep:
+                        print("Erreurs multiples, recalibrage...\n\n")
+                    error = 0
+                    hauteur = mesure
+                    dist1 = mesure
+                    dist2 = mesure
+                    dist3 = mesure
+                    timer_sensor.init(mode=Timer.PERIODIC, period=timer_period_ms, callback=interrupt_hauteur)
+                    
+                return hauteur
+            
+        else:
+            # Donnée lue mais en dehors des limites du capteur
+            count += 1
+            print("[Adresse: {}]\nHors limite ({} cm)\n\n".format(hex(TFMINI_ADDR), mesure))
+            
+            return hauteur
+            
+def interrupt_hauteur(timer):
+    global mesure_hauteur
+    mesure_hauteur = 1
+    return mesure_hauteur
+
+
+# ******************   SOUS PROGRAMMES INTERRUPTION ****************
+# interruption matérielle uart 2 :
+def uart_handler(uart):    
+    global reception_flag  # Pas de traitement long ici !
+    reception_flag = True  # Présence données reçues sur RX !
+
+# ******************   SOUS PROGRAMMES *****************************
+def print_config(config):   # Afficher la configuration du module LoRa
+    print("=== CONFIGURATION du module LoRa ===")
+    for key in ['HEAD', 'ADDR_H', 'ADDR_L', 'SPED','CHANNEL','OPTION']:
+        print(f"{key}: {config[key]}")
+    print("\n")
+
+# ****************   PROGRAMME PRINCIPAL *******************************
+
+# Vérification de la configuration du module : attention pour changer de configuration
+# il faut rebooter le module !
+
+print("\nTest de configuration du module LORA du barrage de commande :\n")
+config = lora_barrage.get_configuration()
+if config:
+    print_config(config)
+else:
+    print(" --> Erreur ou timeout")  # PB liaison série ou autre...
+
+# **** Activation des interruption ****
+uart.irq(handler=uart_handler, trigger=UART.IRQ_RXIDLE)  # Active IRQ sur RX
+timer_sensor = Timer(0)
+timer_sensor.init(mode=Timer.PERIODIC, period=timer_period_ms, callback=interrupt_hauteur)
+
+print("Réception activée, attente...")
+
+# **** Communication en boucle ****
+# Trame reçue de la forme: $ val1 # val2 # texte1 # crc16
+# Trame émise de la forme: $ val3 # val4 # texte2 # crc16
+
+compteur_trames_valides,compteur_trames_invalides = 0, 0
+compteur_trames_emises = 0S
+
+
+while True:
+    print("tamoul")
+    if reception_flag :
+        print("tarace")
+        print(Color.Y)
+        trame = lora_barrage.lecture_trame()
+        if isinstance(trame, dict):
+            for key in ['val1', 'val2', 'texte1', 'crc16']:
+                print(f"\t * {key}: {trame[key]}")  # On affiche les données reçues
+            compteur_trames_valides += 1
+            print(Color.G)
+            if mesure_hauteur == 1:
+                hauteur = use_data()
+                mesure_hauteur = 0
+            trame_recu = lora_barrage.get_trame(hauteur)
+            lora_barrage.envoyer_trame(addr_high_pupitre, addr_low_pupitre, CANAL, trame_recu)
+            compteur_trames_emises += 1   # On compte les trames envoyées
+            print(f"Nombre de trames envoyées par le module barrage : {compteur_trames_emises}")
+            print(Color.Y)
+        else :
+            print("\t * trame non valide !")   # trame non valide à rejeter
+            compteur_trames_invalides += 1
+        reception_flag = False   # prochaines données en attente par flag interruption
+        print(Color.B)
+        print(f"Nombre de trames valides reçues du barrage : {compteur_trames_valides}")
+        print(f"Nombre de trames non valides reçues du barrage : {compteur_trames_invalides}")
+        time.sleep_ms(100)
